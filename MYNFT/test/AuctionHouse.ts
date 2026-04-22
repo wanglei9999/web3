f9b963a1a8fd9eb3b5ee4a0109c33b12acf13281
wanglei999 <wang_lei_it@163.com>
2026-04-22 21:34:37 +0800

    hardhat2

--- MYNFT/test/AuctionHouse.ts
+++ MYNFT/test/AuctionHouse.ts
@@ -16,11 +16,9 @@ describe("AuctionHouse", function () {
 
     const MyNFT = await ethers.getContractFactory("MyNFT");
     myNFT = await MyNFT.deploy();
-    await myNFT.waitForDeployment();
 
     const MyERC20 = await ethers.getContractFactory("MyERC20");
     myERC20 = await MyERC20.deploy();
-    await myERC20.waitForDeployment();
 
     const ETH_USD_FEED = "0x694AA1769357215DE4FAC081bf1f309aDC325306";
 
@@ -28,35 +26,34 @@ describe("AuctionHouse", function () {
     auctionHouse = await upgrades.deployProxy(AuctionHouse, [ETH_USD_FEED], {
       initializer: "initialize",
     });
-    await auctionHouse.waitForDeployment();
 
     await myNFT.safeMint(await seller.getAddress(), "https://example.com/nft/1");
-    await myNFT.connect(seller).setApprovalForAll(await auctionHouse.getAddress(), true);
+    await myNFT.connect(seller).setApprovalForAll(auctionHouse.address, true);
 
-    await myERC20.mint(await bidder1.getAddress(), ethers.parseUnits("1000", 18));
-    await myERC20.mint(await bidder2.getAddress(), ethers.parseUnits("1000", 18));
+    await myERC20.mint(await bidder1.getAddress(), ethers.utils.parseUnits("1000", 18));
+    await myERC20.mint(await bidder2.getAddress(), ethers.utils.parseUnits("1000", 18));
   });
 
   describe("createAuction", function () {
     it("should create an ETH auction", async function () {
       const tokenId = 0;
-      const startingPrice = ethers.parseEther("1");
+      const startingPrice = ethers.utils.parseEther("1");
       const startTime = 0;
       const duration = 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         tokenId,
         startingPrice,
         startTime,
         duration,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
       const auction = await auctionHouse.auctionData(0);
       expect(auction.seller).to.equal(await seller.getAddress());
-      expect(auction.nftContract).to.equal(await myNFT.getAddress());
+      expect(auction.nftContract).to.equal(myNFT.address);
       expect(Number(auction.tokenId)).to.equal(tokenId);
       expect(auction.startingPrice).to.equal(startingPrice);
       expect(auction.isActive).to.be.true;
@@ -65,69 +62,69 @@ describe("AuctionHouse", function () {
 
     it("should create an ERC20 auction", async function () {
       const tokenId = 0;
-      const startingPrice = ethers.parseUnits("100", 18);
+      const startingPrice = ethers.utils.parseUnits("100", 18);
       const startTime = 0;
       const duration = 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         tokenId,
         startingPrice,
         startTime,
         duration,
-        await myERC20.getAddress(),
+        myERC20.address,
         false
       );
 
       const auction = await auctionHouse.auctionData(0);
-      expect(auction.paymentToken).to.equal(await myERC20.getAddress());
+      expect(auction.paymentToken).to.equal(myERC20.address);
       expect(auction.isETH).to.be.false;
     });
 
     it("should track NFT to auction mapping", async function () {
       const tokenId = 0;
-      const startingPrice = ethers.parseEther("1");
+      const startingPrice = ethers.utils.parseEther("1");
       const startTime = 0;
       const duration = 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         tokenId,
         startingPrice,
         startTime,
         duration,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
-      const auctionId = await auctionHouse.nftToken2AuctionId(await myNFT.getAddress(), tokenId);
+      const auctionId = await auctionHouse.nftToken2AuctionId(myNFT.address, tokenId);
       expect(Number(auctionId)).to.equal(0);
     });
 
     it("should prevent duplicate auction for same NFT", async function () {
       const tokenId = 0;
-      const startingPrice = ethers.parseEther("1");
+      const startingPrice = ethers.utils.parseEther("1");
       const startTime = 0;
       const duration = 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         tokenId,
         startingPrice,
         startTime,
         duration,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
       try {
         await auctionHouse.connect(seller).createAuction(
-          await myNFT.getAddress(),
+          myNFT.address,
           tokenId,
           startingPrice,
           startTime,
           duration,
-          ethers.ZeroAddress,
+          ethers.constants.AddressZero,
           true
         );
         expect.fail("Should have reverted");
@@ -140,37 +137,37 @@ describe("AuctionHouse", function () {
   describe("bidAuction", function () {
     it("should place an ETH bid", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
-      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.parseEther("2") });
+      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.utils.parseEther("2") });
 
       const auction = await auctionHouse.auctionData(0);
       expect(auction.highestBidder).to.equal(await bidder1.getAddress());
-      expect(auction.highestBid).to.equal(ethers.parseEther("2"));
+      expect(auction.highestBid).to.equal(ethers.utils.parseEther("2"));
     });
 
     it("should reject bids before auction starts", async function () {
       const futureTime = Math.floor(Date.now() / 1000) + 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         futureTime,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
       try {
-        await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.parseEther("2") });
+        await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.utils.parseEther("2") });
         expect.fail("Should have reverted");
       } catch (e) {
         expect(e).to.exist;
@@ -179,19 +176,19 @@ describe("AuctionHouse", function () {
 
     it("should reject low ETH bids", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
-      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.parseEther("2") });
+      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.utils.parseEther("2") });
 
       try {
-        await auctionHouse.connect(bidder2).bidAuction(0, { value: ethers.parseEther("1.5") });
+        await auctionHouse.connect(bidder2).bidAuction(0, { value: ethers.utils.parseEther("1.5") });
         expect.fail("Should have reverted");
       } catch (e) {
         expect(e).to.exist;
@@ -200,63 +197,63 @@ describe("AuctionHouse", function () {
 
     it("should refund previous bidder", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
       const initialBalance = await ethers.provider.getBalance(await bidder1.getAddress());
 
-      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.parseEther("2") });
-      await auctionHouse.connect(bidder2).bidAuction(0, { value: ethers.parseEther("3") });
+      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.utils.parseEther("2") });
+      await auctionHouse.connect(bidder2).bidAuction(0, { value: ethers.utils.parseEther("3") });
 
       const finalBalance = await ethers.provider.getBalance(await bidder1.getAddress());
-      const diff = initialBalance - finalBalance;
-      expect(diff < ethers.parseEther("0.1")).to.be.true;
+      const diff = initialBalance.sub(finalBalance);
+      expect(diff.lt(ethers.utils.parseEther("0.1"))).to.be.true;
     });
   });
 
   describe("bidAuctionERC20", function () {
     it("should place an ERC20 bid", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseUnits("100", 18),
+        ethers.utils.parseUnits("100", 18),
         0,
         3600,
-        await myERC20.getAddress(),
+        myERC20.address,
         false
       );
 
-      await myERC20.connect(bidder1).approve(await auctionHouse.getAddress(), ethers.parseUnits("200", 18));
-      await auctionHouse.connect(bidder1).bidAuctionERC20(0, ethers.parseUnits("200", 18));
+      await myERC20.connect(bidder1).approve(auctionHouse.address, ethers.utils.parseUnits("200", 18));
+      await auctionHouse.connect(bidder1).bidAuctionERC20(0, ethers.utils.parseUnits("200", 18));
 
       const auction = await auctionHouse.auctionData(0);
       expect(auction.highestBidder).to.equal(await bidder1.getAddress());
-      expect(auction.highestBid).to.equal(ethers.parseUnits("200", 18));
+      expect(auction.highestBid).to.equal(ethers.utils.parseUnits("200", 18));
     });
 
     it("should reject low ERC20 bids", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseUnits("100", 18),
+        ethers.utils.parseUnits("100", 18),
         0,
         3600,
-        await myERC20.getAddress(),
+        myERC20.address,
         false
       );
 
-      await myERC20.connect(bidder1).approve(await auctionHouse.getAddress(), ethers.parseUnits("200", 18));
-      await auctionHouse.connect(bidder1).bidAuctionERC20(0, ethers.parseUnits("200", 18));
+      await myERC20.connect(bidder1).approve(auctionHouse.address, ethers.utils.parseUnits("200", 18));
+      await auctionHouse.connect(bidder1).bidAuctionERC20(0, ethers.utils.parseUnits("200", 18));
 
-      await myERC20.connect(bidder2).approve(await auctionHouse.getAddress(), ethers.parseUnits("150", 18));
+      await myERC20.connect(bidder2).approve(auctionHouse.address, ethers.utils.parseUnits("150", 18));
       try {
-        await auctionHouse.connect(bidder2).bidAuctionERC20(0, ethers.parseUnits("150", 18));
+        await auctionHouse.connect(bidder2).bidAuctionERC20(0, ethers.utils.parseUnits("150", 18));
         expect.fail("Should have reverted");
       } catch (e) {
         expect(e).to.exist;
@@ -267,16 +264,16 @@ describe("AuctionHouse", function () {
   describe("endAuction", function () {
     it("should end auction with winner", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         100,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
-      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.parseEther("2") });
+      await auctionHouse.connect(bidder1).bidAuction(0, { value: ethers.utils.parseEther("2") });
 
       await ethers.provider.send("evm_increaseTime", [101]);
       await ethers.provider.send("evm_mine", []);
@@ -290,12 +287,12 @@ describe("AuctionHouse", function () {
 
     it("should return NFT to seller if no bids", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         100,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
@@ -309,12 +306,12 @@ describe("AuctionHouse", function () {
 
     it("should clear NFT to auction mapping after end", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         100,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
@@ -323,7 +320,7 @@ describe("AuctionHouse", function () {
 
       await auctionHouse.connect(owner).endAuction(0);
 
-      const auctionId = await auctionHouse.nftToken2AuctionId(await myNFT.getAddress(), 0);
+      const auctionId = await auctionHouse.nftToken2AuctionId(myNFT.address, 0);
       expect(Number(auctionId)).to.equal(0);
     });
   });
@@ -333,12 +330,12 @@ describe("AuctionHouse", function () {
       const futureTime = Math.floor(Date.now() / 1000) + 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         futureTime,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
@@ -351,12 +348,12 @@ describe("AuctionHouse", function () {
 
     it("should reject cancellation after auction starts", async function () {
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         0,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
@@ -372,12 +369,12 @@ describe("AuctionHouse", function () {
       const futureTime = Math.floor(Date.now() / 1000) + 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         futureTime,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
@@ -393,18 +390,18 @@ describe("AuctionHouse", function () {
       const futureTime = Math.floor(Date.now() / 1000) + 3600;
 
       await auctionHouse.connect(seller).createAuction(
-        await myNFT.getAddress(),
+        myNFT.address,
         0,
-        ethers.parseEther("1"),
+        ethers.utils.parseEther("1"),
         futureTime,
         3600,
-        ethers.ZeroAddress,
+        ethers.constants.AddressZero,
         true
       );
 
       await auctionHouse.connect(seller).cancelAuction(0);
 
-      const auctionId = await auctionHouse.nftToken2AuctionId(await myNFT.getAddress(), 0);
+      const auctionId = await auctionHouse.nftToken2AuctionId(myNFT.address, 0);
       expect(Number(auctionId)).to.equal(0);
     });
   });
@@ -412,7 +409,7 @@ describe("AuctionHouse", function () {
   describe("UUPS Upgrade", function () {
     it("should upgrade contract", async function () {
       const AuctionHouseV2 = await ethers.getContractFactory("AuctionHouseV2");
-      const upgraded = await upgrades.upgradeProxy(await auctionHouse.getAddress(), AuctionHouseV2);
+      const upgraded = await upgrades.upgradeProxy(auctionHouse.address, AuctionHouseV2);
 
       expect(await upgraded.getVersion).to.exist;
     });
